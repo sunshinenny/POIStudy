e52c2a2910944e702142c9ae4f0755499ac1cb53
SunShinenny <sunshinenny@icloud.com>
2018-07-16 08:41:08 +0800

    实现了空字段的过滤

--- src/com/test/projectCheckListTest.java
+++ src/com/test/projectCheckListTest.java
@@ -72,7 +72,9 @@ public class projectCheckListTest {
 			 */
 			Row headRow = sheet.getRow(0);// 获取表头行
 			for (int i = 0; i < headRow.getLastCellNum(); i++) {
-				originExcelHeadString.add(headRow.getCell(i).toString());
+				// 只有不为空的字段才会被加入匹配列表,实现去除空字段
+				if (headRow.getCell(i)!=null)
+					originExcelHeadString.add(headRow.getCell(i).toString());
 				// 循环结束后,表头字段存储完毕
 			}
 
@@ -84,6 +86,8 @@ public class projectCheckListTest {
 			// 实现同样的功能，且保证顺序一致性。
 			originExcelHeadString = new ArrayList<String>(new TreeSet<String>(originExcelHeadString));
 
+			// feature->高级去重功能,可以先将字段名和判断后的类别组成字典,通过字典自己去重,即键值对的形式
+			
 			// 输出查看获取的表头元素
 			System.out.println(originExcelHeadString);
 
@@ -126,46 +130,6 @@ public class projectCheckListTest {
 				}
 			}
 
-			// 开始循环遍历**行**，表头不处理，故从1开始
-			for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-				tic = new ticket();// 实例化ticket对象
-				// HSSFRow row = sheet.getRow(i);// 获取行对象
-				Row row = sheet.getRow(i);// 获取行对象
-				if (row == null) {// 如果为空，不处理
-					continue;
-				}
-				// 循环遍历单元格
-				for (int j = 0; j < row.getLastCellNum(); j++) {
-					// XSSFCell cell = row.getCell(j);// 获取单元格对象
-					Cell cell = row.getCell(j);// 获取单元格对象
-					if (cell == null) {// 单元格为空设置cellStr为空串
-						cellStr = "";
-					} else if (cell.getCellType() == HSSFCell.CELL_TYPE_BOOLEAN) {// 对布尔值的处理
-						cellStr = String.valueOf(cell.getBooleanCellValue());
-					} else if (cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC) {// 对数字值的处理
-						cellStr = cell.getNumericCellValue() + "";
-					} else {// 其余按照字符串处理
-						cellStr = cell.getStringCellValue();
-					}
-					// 下面按照数据出现位置封装到bean中
-					if (j == 0) {
-						tic.setId(cellStr);
-					} else if (j == 1) {
-						tic.setDate(cellStr);
-					} else if (j == 2) {
-						tic.setFrom(cellStr);
-					} else if (j == 3) {
-						tic.setEnd(cellStr);
-					} else if (j == 4) {
-						tic.setNumber(cellStr);
-					} else if (j == 5) {
-						tic.setPrice(new Float(cellStr).floatValue());
-					} else {
-						tic.setNote(cellStr);
-					}
-				}
-				ticketList.add(tic);// 数据装入List
-			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InvalidFormatException e) {
